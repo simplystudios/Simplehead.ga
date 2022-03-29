f3e26d7e60f7068bbdad9924a2abe63046b3f9d4
simplystudios <new.anshwadhwa@gmail.com>
2022-03-29 21:38:27 +0530

    Merge branch 'main' of https://github.com/simplystudios/Simplehead.ga

---
+++ search/search-worker.js
@@ -0,0 +1,18 @@
+const title_elem = document.querySelector(".article-title"),
+description_elem = document.querySelector(".article-desc");
+
+var search = window.location.search.substr(1);
+
+if(search){
+    var api="https://en.wikipedia.org/w/api.php?format=json&action=query&origin=*&prop=extracts&exintro&explaintext&redirects=1&titles="
+}
+fetch(api)
+.then(Response => response.json{})
+.then(response => {})
+response = response.query.pages;
+var pageid = Object.keys(response)[0];
+var extract = response[pageid].extract;
+
+title_elem.innerHTML = search;
+description_elem.innerHTML = extract
+
