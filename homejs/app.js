a7909f5b5cce4d984860b280330d46f392b97cad
simplystudios <new.anshwadhwa@gmail.com>
2022-03-30 14:53:25 +0530

    new

--- homejs/app.js
+++ homejs/app.js
@@ -5,10 +5,10 @@ var search = window.location.search.substr(1);
 
 if(search){
     var api="https://en.wikipedia.org/w/api.php?format=json&action=query&origin=*&prop=extracts&exintro&explaintext&redirects=1&titles=google" + search;
-}
+
 fetch(api)
 .then(Response => response.json{})
-.then(response => {})
+.then(response => {
 response = response.query.pages;
 var pageid = Object.keys(response)[0];
 var extract = response[pageid].extract;
@@ -16,3 +16,5 @@ var extract = response[pageid].extract;
 title_elem.innerHTML = search;
 description_elem.innerHTML = extract;
 
+})
+}
