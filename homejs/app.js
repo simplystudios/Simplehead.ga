118207b2d40b6eca5df5082f86e86cf086bc5a20
simplystudios <new.anshwadhwa@gmail.com>
2022-03-30 13:40:21 +0530

    new wiki

--- homejs/app.js
+++ homejs/app.js
@@ -4,7 +4,7 @@ description_elem = document.querySelector(".article-desc");
 var search = window.location.search.substr(1);
 
 if(search){
-    var api="https://en.wikipedia.org/w/api.php?format=json&action=query&origin=*&prop=extracts&exintro&explaintext&redirects=1&titles="
+    var api="https://en.wikipedia.org/w/api.php?format=json&action=query&origin=*&prop=extracts&exintro&explaintext&redirects=1&titles=google" + search;
 }
 fetch(api)
 .then(Response => response.json{})
@@ -14,5 +14,5 @@ var pageid = Object.keys(response)[0];
 var extract = response[pageid].extract;
 
 title_elem.innerHTML = search;
-description_elem.innerHTML = extract
+description_elem.innerHTML = extract;
 
