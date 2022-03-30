f8bdbfdea97164ac9fd249acff896ed5d1a50c4c
simplystudios <new.anshwadhwa@gmail.com>
2022-03-30 14:53:29 +0530

    new

--- homejs/index.js
+++ homejs/index.js
@@ -1,11 +1,11 @@
 const searchinput = document.querySelector(".search-input");
 
-searchinput.addEventListener("keyup", function(e){
-if(e.keycode === 13){
+searchInput.addEventListener("keyup",function(e){
+if(e.keyCode === 13){
     var value = searchinput.value;
     if(value){
-        window.location.href = "/read.html?/" + value;
-    }
-}
+        window.location.href = "read.html?" + value;
+     }
+   }
 
 })
